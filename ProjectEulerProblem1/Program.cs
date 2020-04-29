4db20d66865e9a263dddb587c4e317ecf66218b1
ryanjavens <r.c.javens@gmail.com>
2020-04-28 22:57:51 -0400

    Success!!

--- ProjectEulerProblem1/Program.cs
+++ ProjectEulerProblem1/Program.cs
@@ -6,7 +6,24 @@ namespace ProjectEulerProblem1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.
+            // Find the sum of all the multiples of 3 or 5 below 1000.
+
+            int sum = 0;
+            int max = 1000;
+            for(int i = 1; i < max; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    sum += i;
+                }
+                else if(i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Sum is {sum}");
         }
     }
 }
